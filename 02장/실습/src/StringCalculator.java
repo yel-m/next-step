a6bd275b56db3447c4153585b2abe5ece3092600
yel-m <coin6442@gmail.com>
2024-01-11 23:59:12 +0900

    add : 기본 & 커스텀 구분자로 문자열 구분하기

--- 02장/실습/src/StringCalculator.java
+++ 02장/실습/src/StringCalculator.java
@@ -1,6 +1,18 @@
 public class StringCalculator {
-    int add(String str) {
+    int add(String str) throws NullPointerException{
+        int sum = 0;
+        String[] numbers;
 
-        return 0;
+        if(str.substring(0, 2) == "\\" && str.substring(3, 5) == "\n") {
+            String customSeparator = String.valueOf(str.charAt(2));
+            numbers = str.split(customSeparator);
+        } else {
+            numbers = str.split("[:,]");
+        }
+        
+        for (int i = 0; i < numbers.length; i++) {
+            sum += Integer.parseInt(numbers[i]);
+        }
+        return sum;
     }
 }
