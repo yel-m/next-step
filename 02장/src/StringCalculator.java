ebabba034bbe92d56ef35f842ad450776ef5f617
yel-m <coin6442@gmail.com>
2024-01-17 00:29:45 +0900

    fix : matcher를 사용해 커스텀 구분자 추출 & 구분된 값들의 합 계산하는 함수 매개변수 수정

--- 02장/src/StringCalculator.java
+++ 02장/src/StringCalculator.java
@@ -1,19 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     int add(String str) {
-        if (str.indexOf("\n") - str.indexOf("//") == 3) {
-            String customSeparator = String.valueOf(str.charAt(str.indexOf("\n") - str.indexOf("//") - 1));
-            str = str.substring(str.indexOf("\n") + 1).replace(customSeparator, ",");
-            return calculateSumWithDelimiter(str);
+        String[] separatedValues;
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            separatedValues = matcher.group(2).split(customDelimiter);
+            return calculateSumFromSeparatedValues(separatedValues);
         } else {
-            return calculateSumWithDelimiter(str);
+            separatedValues = str.split("[:,]");
+            return calculateSumFromSeparatedValues(separatedValues);
         }
     }
 
-    int calculateSumWithDelimiter(String str) {
+    int calculateSumFromSeparatedValues(String[] separatedValues) {
         int sum = 0;
-        String[] separatedStrings = str.split("[:,]");
-        for (String separatedString : separatedStrings) {
-            int number = changeStringToInteger(separatedString);
+        for (String value : separatedValues) {
+            int number = changeStringToInteger(value);
             if(number < 0)
                 throw new RuntimeException("음수를 입력해야 합니다.");
             else
