1c8f19f6886144119d2fbdc39c56b28ae15136de
yel-m <coin6442@gmail.com>
2024-01-25 11:55:00 +0900

    add : HTTP 요청 정보 전체 출력

--- 03장/src/main/java/webserver/RequestHandler.java
+++ 03장/src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +16,22 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    @Override
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ArrayList<String> requestInfos = new ArrayList<>();
+            String info;
+            while(!"".equals(info = reader.readLine())) {
+                if (info == null) { return; } // NullPointerException 처리 안해도 되나?
+                requestInfos.add(info);
+            }
+            for(String requestInfo : requestInfos) {
+                System.out.println(requestInfo);
+            }
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
