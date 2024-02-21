df3e7df4db5315c60d7bb94b83a019347cf91000
yel-m <coin6442@gmail.com>
2024-02-21 10:50:08 +0900

    test : isUser 기능 테스트 & 예외 처리 테스트 분리

--- 03장/src/test/java/util/ModelUtilTest.java
+++ 03장/src/test/java/util/ModelUtilTest.java
@@ -40,11 +40,13 @@ public class ModelUtilTest {
         Map<String, String> inputs1 = HttpRequestUtils.parseQueryString(input1);
 
         assertTrue(isUser(inputs1.get("userId"), inputs1.get("password")));
+    }
 
-        // 올바르지 않게 입력했을 때
-        String input2 = "userId=hihihi&password=password2&email=coin6442&name=yelim";
-        Map<String, String> inputs2 = HttpRequestUtils.parseQueryString(input2);
+    @Test(expected=UserNotFoundException.class)
+    public void invokeUserNotFoundException() {
+        String input2 = "userId=yelim&password=password2&email=coin6442&name=yelim";
+        Map<String, String> inputs1 = HttpRequestUtils.parseQueryString(input2);
 
-        assertFalse(isUser(inputs2.get("userId"), inputs2.get("password")));
+        isUser(inputs1.get("userId"), inputs1.get("password"));
     }
 }
