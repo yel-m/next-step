cd03f532d6b4cfdc7de05e1d5e46b912d9a16303
Jung Ye Lim <101961939+yel-m@users.noreply.github.com>
2024-08-15 13:14:23 +0900

    03장 로그인하기 (#5)

--- 03장/src/main/java/util/ModelUtils.java
+++ 03장/src/main/java/util/ModelUtils.java
@@ -4,13 +4,23 @@ import model.User;
 
 import java.util.Map;
 
+import static db.DataBase.findUserById;
+
 public class ModelUtils {
-    public static void createUser(Map<String, String> parameters) {
-        new User(
+    public static User createUser(Map<String, String> parameters) {
+        return new User(
                 parameters.get("userId"),
                 parameters.get("password"),
                 parameters.get("name"),
                 parameters.get("email")
         );
+    }
+
+    public static boolean isUser(String userId, String password) {
+        User user = findUserById(userId);
+        if(user == null || !user.getPassword().equals(password)) {
+            throw new UserNotFoundException();
+        }
+        return true;
     }
 }
